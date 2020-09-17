fdecbc1394bed521556d9bf0f53d9fdc20e4332d
Erick Merchant <erickmerchant@gmail.com>
2020-09-16 23:43:07 -0400

    Add all the day, month, and year stuff

--- src/main.js
+++ src/main.js
@@ -1,13 +1,72 @@
 import {html} from '@erickmerchant/framework/main.js'
 
+const [, year, month] = window.location.pathname.split('/')
+
 const now = new Date()
 
+const date = new Date(
+  year != null ? Number(year) : now.getFullYear(),
+  month != null ? Number(month) - 1 : now.getMonth()
+)
+
 export const defaultState = {
-  date: now
+  date
 }
 
 export const createComponent = ({app, classes}) => {
   return (state) => html`
-    <body class=${classes.app}></body>
+    <body class=${classes.app}>
+      <header class=${classes.top}>
+        <h1 class=${classes.h1}>
+          ${state.date.toLocaleDateString('en-US', {
+            month: 'long',
+            year: 'numeric'
+          })}
+        </h1>
+      </header>
+      <main class=${classes.bottom}>
+        <div class=${classes.grid}>
+          <div class=${classes.weekDay}>Sunday</div>
+          <div class=${classes.weekDay}>Monday</div>
+          <div class=${classes.weekDay}>Tuesday</div>
+          <div class=${classes.weekDay}>Wednesday</div>
+          <div class=${classes.weekDay}>Thursday</div>
+          <div class=${classes.weekDay}>Friday</div>
+          <div class=${classes.weekDay}>Saturday</div>
+          ${{
+            *[Symbol.iterator]() {
+              const year = state.date.getFullYear()
+              const month = state.date.getMonth()
+              const daysInTheMonth = new Date(year, month + 1, 0).getDate()
+
+              let dayOfWeek = new Date(year, month, 1).getDay()
+              let weekOfMonth = 0
+              let i = 1
+
+              do {
+                yield html`
+                  <div
+                    class=${classes.day}
+                    style=${`--x: ${dayOfWeek + 1}; --y: ${weekOfMonth + 2};`}
+                  >
+                    ${i}
+                  </div>
+                `
+
+                i++
+
+                dayOfWeek++
+
+                if (dayOfWeek > 6) {
+                  weekOfMonth++
+
+                  dayOfWeek = 0
+                }
+              } while (i <= daysInTheMonth)
+            }
+          }}
+        </div>
+      </main>
+    </body>
   `
 }
--- src/styles.js
+++ src/styles.js
@@ -4,7 +4,6 @@ export const _start = `
     margin: 0;
     padding: 0;
     max-width: 100%;
-    border-radius: 3px;
     font: inherit;
   }
 
@@ -16,8 +15,47 @@ export const _start = `
 export const styles = {
   app: `
     display: grid;
+    background-color: hsl(200, 0%, 90%);
+    color: hsl(200, 0%, 10%);
+    font: 16px/1.5 system-ui;
+    display: grid;
+    grid-template-rows: 1fr 1fr;
     height: 100%;
-    background-color: hsl(200, 5%, 60%);
-    font: 24px/1.5 Consolas, monaco, monospace;
+    overflow: visible;
+    margin: 1em;
+  `,
+  top: `
+    grid-row: 1;
+  `,
+  bottom: `
+    grid-row: 2;
+  `,
+  h1: `
+    font-weight: bold;
+    font-size: 3em;
+  `,
+  grid: `
+    display: grid;
+    grid-template-columns: repeat(7, calc((100% - 3em) / 7));
+    gap: 0.5em;
+    padding-bottom: 1em;
+  `,
+  weekDay: `
+    text-align: center;
+    padding: 1em;
+    font-weight: bold;
+    grid-row: 1;
+  `,
+  day: `
+    grid-column: var(--x);
+    grid-row: var(--y);
+    border: 1px solid currentColor;
+    background: white;
+    height: 0;
+    overflow: visible;
+    padding-top: 0.5em;
+    padding-right: 0.5em;
+    padding-left: 0.5em;
+    padding-bottom: 100%;
   `
 }
